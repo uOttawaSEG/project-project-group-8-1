52d304cfed6bb0fafa171df5bc1490d556f13c81
Faizaan <faizaantm@gmail.com>
2025-09-28 14:35:06 -0400

    Added Tutor.java class skeleton

---
+++ app/src/main/java/com/example/seg2105_d1/Tutor.java
@@ -0,0 +1,43 @@
+package com.example.seg2105_d1;
+
+import java.util.ArrayList;
+
+public class Tutor extends User{
+
+    private String highestDegree;
+    private ArrayList<String> coursesOffered;
+
+    public Tutor() {
+        super();
+        this.highestDegree = null;
+        this.coursesOffered = null;
+    }
+
+    public String getHighestDegree() {
+        return this.highestDegree;
+    }
+
+    public void setHighestDegree(String highestDegree){
+        this.highestDegree = highestDegree;
+    }
+
+    public ArrayList<String> getCoursesOffered(){
+        return this.coursesOffered;
+    }
+
+    public void setCoursesOffered(){
+        //add courses to coursesOffered arrayList; needs ui consideration
+    }
+
+    //temp structure for register method
+    @Override
+    public void register(User u) {
+
+        if(checkDuplicates(u.getEmailAddressUsername())){
+            return;
+        }
+
+        userList.add(u);
+    }
+
+}
