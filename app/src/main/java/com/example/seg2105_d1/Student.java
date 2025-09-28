90510366a0d385ff72c83b3a260cf98a1d3f883a
Mingyu <mingyuw1937@gmail.com>
2025-09-28 14:04:13 -0400

    Created student.java class

---
+++ app/src/main/java/com/example/seg2105_d1/Student.java
@@ -0,0 +1,23 @@
+package com.example.seg2105_d1;
+
+public class Student extends User{
+    private String programOfStudy;
+
+    public Student(String programOfStudy) {
+        this.programOfStudy = programOfStudy;
+    }
+
+    public String getProgramOfStudy() { return this.programOfStudy; }
+
+    public void setProgramOfStudy(String programOfStudy) { this.programOfStudy = programOfStudy; }
+
+    @Override
+    public void register(User u) {
+        if(!checkDuplicates(u.getEmailAddressUsername())) {
+            return;
+        }
+
+        userList.add(u);
+        //show success message
+    }
+}
