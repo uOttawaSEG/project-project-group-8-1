d811db6cd2f59f8b005b10877d78f48528859a18
Faizaan <faizaantm@gmail.com>
2025-11-09 11:46:44 -0500

    session and availability class general structure refined and added

---
+++ app/src/main/java/com/example/seg2105_d1/Model/Availability.java
@@ -0,0 +1,102 @@
+package com.example.seg2105_d1.Model;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class Availability {
+    //instance variables ------------------------------------------------------------
+    private LocalTime startTime;
+    private LocalTime endTime;
+    private LocalDate date;
+
+    private Tutor tutor;
+
+    //constructors ------------------------------------------------------------------
+    public Availability() {
+
+    }
+
+    //getters and setters -----------------------------------------------------------
+    public LocalTime getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(String startTime) {
+        this.startTime = LocalTime.parse(startTime);;
+    }
+
+    public LocalTime getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(String endTime) {
+        this.endTime = LocalTime.parse(endTime);
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = LocalDate.parse(date);
+    }
+
+    public Tutor getTutor() {
+        return tutor;
+    }
+
+    public void setTutor(Tutor tutor)  {
+        this.tutor = tutor;
+    }
+
+    //helper methods ------------------------------------------------------------------
+
+    /**
+     * Checks if sessions are overlapping. Used for verification of new session creation.
+     * @param availabilityA should be an existing session
+     * @param availabilityB should be a new session to create
+     * @return true if sessions don't overlap, false otherwise
+     */
+    public static boolean noOverlap(Availability availabilityA, Availability availabilityB) {
+        LocalDate sessionADate = availabilityA.getDate();
+        LocalDate sessionBDate = availabilityB.getDate();
+        LocalTime sessionAStartTime = availabilityA.getStartTime();
+        LocalTime sessionAEndTime = availabilityA.getEndTime();
+        LocalTime sessionBStartTime = availabilityB.getStartTime();
+        LocalTime sessionBEndTime = availabilityB.getEndTime();
+
+        if(sessionADate.equals(sessionBDate)) {
+            if(sessionBStartTime.isBefore(sessionAEndTime)) {
+                return false;
+            }
+        }
+        return true;
+
+    }
+
+    /**
+     * Checks if start time is before end time. Used for authenticating a new session.
+     * @return true if start time is before end time, false otherwise.
+     */
+    public boolean timeOrderValid() {
+        if(startTime.isBefore(endTime)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Checks if date and time are after the current date and time.
+     * @return true if date and time is after current actual date and time, false otherwise.
+     */
+    public boolean timingValid() {
+        if(date.isAfter(LocalDate.now()) || date.equals(LocalDate.now())) {
+            if(startTime.isAfter(LocalTime.now())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
--- app/src/main/java/com/example/seg2105_d1/Model/Session.java
+++ app/src/main/java/com/example/seg2105_d1/Model/Session.java
@@ -1,53 +1,32 @@
 package com.example.seg2105_d1.Model;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
-
 public class Session {
-    //instance variables ------------------------------------------------------------
-    private LocalTime startTime;
-    private LocalTime endTime;
-    private LocalDate date;
+    //instance variables-------------------------------------------------
+    Availability availability;
+    String course;
+    Student student;
 
-    private Tutor tutor;
-    private Student student;
+    String status;
 
-    //constructors ------------------------------------------------------------------
+    //constructors-------------------------------------------------------
     public Session() {
 
     }
-
-    //getters and setters -----------------------------------------------------------
-    public LocalTime getStartTime() {
-        return startTime;
+     //getters and setters-----------------------------------------------
+    public Availability getAvailability() {
+        return availability;
     }
 
-    public void setStartTime(String startTime) {
-        this.startTime = LocalTime.parse(startTime);;
+    public void setAvailability(Availability availability) {
+        this.availability = availability;
     }
 
-    public LocalTime getEndTime() {
-        return endTime;
+    public String getCourse() {
+        return course;
     }
 
-    public void setEndTime(String endTime) {
-        this.endTime = LocalTime.parse(endTime);
-    }
-
-    public LocalDate getDate() {
-        return date;
-    }
-
-    public void setDate(String date) {
-        this.date = LocalDate.parse(date);
-    }
-
-    public Tutor getTutor() {
-        return tutor;
-    }
-
-    public void setTutor(Tutor tutor)  {
-        this.tutor = tutor;
+    public void setCourse(String course) {
+        this.course = course;
     }
 
     public Student getStudent() {
@@ -58,54 +37,18 @@ public class Session {
         this.student = student;
     }
 
-    //helper methods ------------------------------------------------------------------
-
-    /**
-     * Checks if sessions are overlapping. Used for verification of new session creation.
-     * @param sessionA should be an existing session
-     * @param sessionB should be a new session to create
-     * @return true if sessions don't overlap, false otherwise
-     */
-    public static boolean noOverlap(Session sessionA, Session sessionB) {
-        LocalDate sessionADate = sessionA.getDate();
-        LocalDate sessionBDate = sessionB.getDate();
-        LocalTime sessionAStartTime = sessionA.getStartTime();
-        LocalTime sessionAEndTime = sessionA.getEndTime();
-        LocalTime sessionBStartTime = sessionB.getStartTime();
-        LocalTime sessionBEndTime = sessionB.getEndTime();
-
-        if(sessionADate.equals(sessionBDate)) {
-            if(sessionBStartTime.isBefore(sessionAEndTime)) {
-                return false;
-            }
-        }
-        return true;
-
+    public String getStatus() {
+        return status;
     }
 
-    /**
-     * Checks if start time is before end time. Used for authenticating a new session.
-     * @return true if start time is before end time, false otherwise.
-     */
-    public boolean timeOrderValid() {
-        if(startTime.isBefore(endTime)) {
-            return true;
-        } else {
-            return false;
-        }
+    //possible statuses: PENDING, APPROVED REJECTED
+    public void setStatus(String status) {
+        this.status = status;
     }
 
-    /**
-     * Checks if date and time are after the current date and time.
-     * @return true if date and time is after current actual date and time, false otherwise.
-     */
-    public boolean timingValid() {
-        if(date.isAfter(LocalDate.now()) || date.equals(LocalDate.now())) {
-            if(startTime.isAfter(LocalTime.now())) {
-                return true;
-            }
-        }
-        return false;
-    }
+
+    //helper methods-------------------------------------------------------
+
+
 
 }
