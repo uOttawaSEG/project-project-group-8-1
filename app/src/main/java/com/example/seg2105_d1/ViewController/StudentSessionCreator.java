9caa9486dc19ab9e3d3728d71526cd89cfa80d86
Mingyu <mingyuw1937@gmail.com>
2025-12-01 22:28:14 -0500

    bug

--- app/src/main/java/com/example/seg2105_d1/ViewController/StudentSessionCreator.java
+++ app/src/main/java/com/example/seg2105_d1/ViewController/StudentSessionCreator.java
@@ -238,11 +238,20 @@ public class StudentSessionCreator extends AppCompatActivity {
         public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
 
             Availability slot = items.get(position);
+            double rate = 0f;
 
             DateTimeFormatter timeFormatSpinner = DateTimeFormatter.ofPattern("yyyy-MM-dd");
             DateTimeFormatter timeFormatDb = DateTimeFormatter.ofPattern("HH:mm");
 
-            holder.tvTutorName.setText("Tutor: " + slot.getTutorName());
+            FirebaseFirestore.getInstance().collection("users")
+                    .document(slot.getTutorId())
+                    .get()
+                    .addOnSuccessListener(documentSnapshot ->
+                            {
+                                rate = documentSnapshot.getDouble("rating").doubleValue();
+                            });
+
+            holder.tvTutorName.setText("Tutor: " + slot.getTutorName() + "   Rating: " + slot.getRating());
             holder.tvCourse.setText("Course: " + courseCode);
             holder.tvDateTime.setText(
                     slot.getDate().format(timeFormatSpinner) + " | " +
